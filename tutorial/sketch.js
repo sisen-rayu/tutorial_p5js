ce24e11a3e348195aa7fa0f8eee984d8b5a5e748
chihiro <chihiro.tanigawa@accenture.com>
2021-11-25 19:53:56 +0900

    円を操作して色塗りできる

--- tutorial/sketch.js
+++ tutorial/sketch.js
@@ -1,8 +1,12 @@
 function setup() {
-
-}
-
-function draw() {
-    //左から50px、上から50pxに幅と高さが80pxの楕円を描くという意味
-    ellipse(50,50,80,80);
-}
+    createCanvas(640, 480);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
